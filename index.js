217976bfc07443a45d8e6427f0b511ebe409701a
Aaron Chu <aaronchu357@gmail.com>
2019-01-29 23:38:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(arrMus, arrIns) {
+  var arr = []
+  for(var i = 0; i < arrMus.length; i++){
+    var string = arrMus[i] + " plays " + arrIns[i]
+    arr.push(string)
+  }
+  return arr
+}
